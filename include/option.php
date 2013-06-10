c8ea739b267c4cb5bbfd17be2a7eec7a99f44413
Lionel POINTET <lionel.pointet@globalis-ms.com>
2013-06-10 15:21:02 +0200

    Feature - Added get_object_taxonomies() function in options class

--- include/option.php
+++ include/option.php
@@ -11,4 +11,13 @@ class WPPD_Option {
         // @TODO: get the list of post types saved in admin
         return get_post_types();
     }
+
+    /**
+     * Returns a list of taxonomies given a post type, for which the plugin will do something
+     * Default: returns the list of all registered taxonomies on this post type
+     */
+    public static function get_object_taxonomies( $post_type ) {
+        // @TODO: get the list of taxonomies saved in admin for that post type
+        return get_object_taxonomies( $post_type );
+    }
 }

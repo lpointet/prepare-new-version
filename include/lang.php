abdeda36499fbd02ab55799349fe77d74abeda49
Lionel POINTET <lionel.pointet@globalis-ms.com>
2013-06-10 17:23:18 +0200

    Bugfix - Renamed 'Duplicata' as it seems to be 'Duplicate' in English

--- include/lang.php
+++ include/lang.php
@@ -8,20 +8,20 @@ if( !function_exists( '_noop' ) ) {
     }
 }
 
-define( 'WPPD_STR_DUPLICATA_META_BOX_TITLE', _x( 'Duplicatas', 'meta box title', WPPD_DOMAIN ) );
-define( 'WPPD_STR_DUPLICATA_COLUMN_TITLE', _x( 'Duplicatas', 'post list column title', WPPD_DOMAIN ) );
-define( 'WPPD_STR_DUPLICATA_STATUS_LABEL', __( 'Duplicata', WPPD_DOMAIN ) );
-define( 'WPPD_STR_DUPLICATA_STATUS_LABEL_COUNT_SINGULAR', _noop( 'Duplicata <span class="count">(%s)</span>' ) );
-define( 'WPPD_STR_DUPLICATA_STATUS_LABEL_COUNT_PLURAL', _noop( 'Duplicatas <span class="count">(%s)</span>' ) );
+define( 'WPPD_STR_DUPLICATA_META_BOX_TITLE', _x( 'Duplicates', 'meta box title', WPPD_DOMAIN ) );
+define( 'WPPD_STR_DUPLICATA_COLUMN_TITLE', _x( 'Duplicates', 'post list column title', WPPD_DOMAIN ) );
+define( 'WPPD_STR_DUPLICATA_STATUS_LABEL', __( 'Duplicate', WPPD_DOMAIN ) );
+define( 'WPPD_STR_DUPLICATA_STATUS_LABEL_COUNT_SINGULAR', _noop( 'Duplicate <span class="count">(%s)</span>' ) );
+define( 'WPPD_STR_DUPLICATA_STATUS_LABEL_COUNT_PLURAL', _noop( 'Duplicates <span class="count">(%s)</span>' ) );
 
 //
 // Meta box
 //
 define( 'WPPD_STR_ORIGINAL_POST', __( 'Original: ', WPPD_DOMAIN ) );
 define( 'WPPD_STR_CURRENT_ORIGINAL_POST', __( 'This is an original', WPPD_DOMAIN ) );
-define( 'WPPD_STR_META_BOX_DUPLICATA_LIST_TITLE', _x( 'Duplicatas', 'duplicatas list title in meta box', WPPD_DOMAIN ) );
+define( 'WPPD_STR_META_BOX_DUPLICATA_LIST_TITLE', _x( 'Duplicates', 'duplicatas list title in meta box', WPPD_DOMAIN ) );
 define( 'WPPD_STR_ERASE_BUTTON', __( 'Replace with this one', WPPD_DOMAIN ) );
-define( 'WPPD_STR_DUPLICATE_BUTTON', __( 'Duplicate', WPPD_DOMAIN ) );
+define( 'WPPD_STR_DUPLICATE_BUTTON', _x( 'Duplicate', 'Duplication button', WPPD_DOMAIN ) );
 define( 'WPPD_STR_COPY_BUTTON', __( 'Create a copy', WPPD_DOMAIN ) );
 define( 'WPPD_STR_PUBLISH_META_BOX_TITLE', __( 'Publish', WPPD_DOMAIN ) );
-define( 'WPPD_STR_SAVE_DUPLICATA_BUTTON', __( 'Save duplicata', WPPD_DOMAIN ) );
+define( 'WPPD_STR_SAVE_DUPLICATA_BUTTON', __( 'Save duplicate', WPPD_DOMAIN ) );

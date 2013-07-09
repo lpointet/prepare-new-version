eaa58c5b49f3197259f7319c452c66ff85736691
Lionel POINTET <lionel.pointet@globalis-ms.com>
2013-07-09 11:36:35 +0200

    Feature - Allow to delete a duplicate

--- template/submit_meta_box.php
+++ template/submit_meta_box.php
@@ -1,3 +1,17 @@
-<p>
-    <input type="submit" name="save" id="publish" value="<?php echo esc_attr( WPPD_STR_SAVE_DUPLICATA_BUTTON ); ?>" class="button button-primary button-large" />
-</p>
+<div class="submitbox">
+    <div id="delete-action">
+    <?php
+    if ( current_user_can( "delete_post", $post->ID ) ) {
+        if ( !EMPTY_TRASH_DAYS )
+            $delete_text = __( 'Delete Permanently' );
+        else
+            $delete_text = __( 'Move to Trash' );
+        ?>
+        <a class="submitdelete deletion" href="<?php echo get_delete_post_link( $post->ID ); ?>"><?php echo $delete_text; ?></a><?php
+    } ?>
+    </div>
+    <div id="publishing-action">
+        <input type="submit" name="save" id="publish" value="<?php echo esc_attr( WPPD_STR_SAVE_DUPLICATA_BUTTON ); ?>" class="button button-primary button-large" />
+    </div>
+    <div class="clear"></div>
+</div>

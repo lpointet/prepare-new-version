e30c5f2722e7ebcb90d2346e0c42906447edab9d
Lionel POINTET <lionel.pointet@globalis-ms.com>
2013-07-10 13:51:40 +0200

    Feature - Added spinner on the submit meta box for duplicates

--- template/submit_meta_box.php
+++ template/submit_meta_box.php
@@ -1,19 +1,22 @@
-<div class="submitbox">
-    <div id="delete-action">
-    <?php
-    if ( current_user_can( "delete_post", $post->ID ) ) {
-        if ( !EMPTY_TRASH_DAYS )
-            $delete_text = __( 'Delete Permanently' );
-        else
-            $delete_text = __( 'Move to Trash' );
-        ?>
-        <a class="submitdelete deletion" href="<?php echo get_delete_post_link( $post->ID ); ?>"><?php echo $delete_text; ?></a><?php
-    } ?>
+<div id="submitpost" class="submitbox">
+    <div id="major-publishing-actions">
+        <div id="delete-action">
+        <?php
+        if ( current_user_can( "delete_post", $post->ID ) ) {
+            if ( !EMPTY_TRASH_DAYS )
+                $delete_text = __( 'Delete Permanently' );
+            else
+                $delete_text = __( 'Move to Trash' );
+            ?>
+            <a class="submitdelete deletion" href="<?php echo get_delete_post_link( $post->ID ); ?>"><?php echo $delete_text; ?></a><?php
+        } ?>
+        </div>
+        <div id="publishing-action">
+            <span class="spinner"></span>
+            <a href="<?php echo add_query_arg( PNV_ACTION_NAME, PNV_ERASE_ACTION, $action_url ); ?>" id="erase" class="button button-primary button-large"><?php echo PNV_STR_ERASE_BUTTON; ?></a>
+            <input type="submit" name="save" id="publish" value="<?php echo esc_attr( PNV_STR_SAVE_DUPLICATA_BUTTON ); ?>" class="button button-large" />
+        </div>
+        <div class="clear"></div>
+        <a href="<?php echo add_query_arg( PNV_ACTION_NAME, PNV_COPY_ACTION , $action_url ); ?>" id="copy" class=""><?php echo PNV_STR_COPY_BUTTON; ?></a>
     </div>
-    <div id="publishing-action">
-        <a href="<?php echo add_query_arg( PNV_ACTION_NAME, PNV_ERASE_ACTION, $action_url ); ?>" id="erase" class="button button-primary button-large"><?php echo PNV_STR_ERASE_BUTTON; ?></a>
-        <input type="submit" name="save" id="publish" value="<?php echo esc_attr( PNV_STR_SAVE_DUPLICATA_BUTTON ); ?>" class="button button-large" />
-    </div>
-    <div class="clear"></div>
-    <a href="<?php echo add_query_arg( PNV_ACTION_NAME, PNV_COPY_ACTION , $action_url ); ?>" id="copy" class=""><?php echo PNV_STR_COPY_BUTTON; ?></a>
 </div>
